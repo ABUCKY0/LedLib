f65ceb05c5c7335fb32278c77a4b80ea16c14192
ABUCKY0 <81783950+ABUCKY0@users.noreply.github.com>
2024-03-14 19:15:27 -0500

    Setup LedLib (Something isn't right tho??)

--- include/LedLib/LedLib.hpp
+++ include/LedLib/LedLib.hpp
@@ -2,146 +2,194 @@
 
 #include "main.h"
 
-namespace LedLib {
+namespace LedLib
+{
 
-    class StripSizeTooLarge : public std::exception {
-        public:
-    char * what () {
+    class StripSizeTooLarge : public std::exception
+    {
+    public:
+        char *what()
+        {
             return "Strip Size is outside specified range 1-64";
         }
     };
 
-    struct RGB {
+    struct RGB
+    {
         uint8_t red;
         uint8_t green;
         uint8_t blue;
-    }
+    };
 
-    struct HSV {
-        double hue;
-        double saturation;
-        double value;
-    }
+    struct HSV
+    {
+        double hue = 0;
+        double saturation = 0;
+        double value = 0;
+    };
 
-    class Led {
-        public:
+    class LedLib
+    {
 
-        const int size;
+    public:
+        int size;
         pros::ADILED strip;
 
-        
         /**
          * @brief Construct a new Led object
-         * 
+         *
+         * @note DO NOT USE. WILL NOT WORK
+         *
+         */
+        LedLib();
+        /**
+         * @brief Construct a new Led object
+         *
          * @param adiport 1-8 representing Triports A-H
          * @param length 1-64 representing the length of the LEDs
          *
          * @throws StripSizeTooLarge if size is > 64
          */
-        Led(uint8_t adiport, int length);
+        LedLib(uint8_t adiport, int length);
 
         /**
          * @brief Construct a new Led object
-         * 
+         *
          * @param smartport 1-21 representing SmartPorts 1-21
          * @param adiport 1-8 representing Triports A-H
          * @param length 1-64 representing the length of the LEDs
          *
          * @throws StripSizeTooLarge if size is > 64
          */
-        Led(uint8_t smartport, uint8_t adiport, int length);
+        LedLib(uint8_t smartport, uint8_t adiport, int length);
 
         /**
          * @brief Set all leds to a given RGB color
-         * 
+         *
          * @param rgb an RGB value represented as a struct
          */
-        setAll(RGB rgb);
+        void setAll(RGB rgb);
 
         /**
          * @brief Set all leds to a given RGB color
-         * 
+         *
          * @param hsv an HSV value represented as a struct
-         *
-         * @note Converts HSV to RGB, then calls setAll(RGB) for simplicity
          */
-        setAll(HSV hsv);
+        void setAll(HSV hsv);
 
         /**
          * @brief Set all LEDs to a specified color loopy style
          *
          * If your LED strip is being b*tchy about setting all the colors at once (like mine do), call this function instead.
-         * 
-         * @param rgb 
+         *
+         * @param rgb
          */
-        setAllButchy(RGB rgb);
+        void setAllButchy(RGB rgb);
 
         /**
          * @brief Set all LEDs to a specified color loopy style
          *
          * If your LED strip is being b*tchy about setting all the colors at once (like mine do), call this function instead.
-         * 
+         *
          * @param hsv a HSV struct
-         * @note Converts to RGB, then calls setAllButchy(RGB) for simplicity
          */
-        setAllButchy(RGB hsv);
+        void setAllButchy(HSV hsv);
 
         /**
          * @brief Set a given pixel to a specified color
-         * 
+         *
          * @param rgb the RGB color to set the strip to
          * @param index the index of the LED
          */
-        setPixel(RGB rgb, uint8_t index);
+        void setPixel(RGB rgb, uint8_t index);
 
         /**
-         * @brief Set a given pixel to a specified color 
-         * 
+         * @brief Set a given pixel to a specified color
+         *
          * @param hsv rgb the RGB color to set the strip to
          * @param index the index of the LED
-         *
-         * @note Converts HSV to RGB, then calls setPixel(RGB) for simplicity
          */
-        setPixel(HSV hsv, uint8_t index);
+        void setPixel(HSV hsv, uint8_t index);
 
         /**
          * @brief Sets the entire RGB strip to a specified Graident
-         * 
+         *
          * @param colora Color Represented as a 6 digit hex code (0xFF00FF)
          * @param colorb Color Represented as a 6 digit hex code (0xFF00FF)
-         *
-         * @note Converts RGB to uint32_t then calls setGraident(uint32_t, uint32_t) for simplicity
          */
-        setGraident(uint32_t colora, uint32_t colorb);
+        void setGraident(uint32_t colora, uint32_t colorb);
 
         /**
          * @brief Sets the entire RGB strip to a specified Graident
          *
          * @param colora Color Represented as an HSV struct
          * @param colorb Color Represented as an HSV struct
-         *
-         * @note Converts RGB to uint32_t then calls setGraident(uint32_t, uint32_t) for simplicity
          */
-        setGraident(HSV colora, HSV colorb);
+        void setGraident(HSV colora, HSV colorb);
 
         /**
          * @brief Sets the entire RGB strip to a specified Graident
-         * 
+         *
          * @param colora Color Represented as an RGB struct
          * @param colorb Color Represented as an RGB struct
-         *
-         * @note Converts RGB to uint32_t then calls setGraident(uint32_t, uint32_t) for simplicity
          */
-        setGraident(RGB colora, RGB colorb);
-
+        void setGraident(RGB colora, RGB colorb);
 
         /// Static Functions
 
         /**
-         * @brief 
-         * 
+         * @brief
+         *
          * @note Credits to https://gist.github.com/peacefixation/5eeb6e992a012ea2f42cd5419df65ea7
          */
-        static lerpRGB(uint32_t color1, uint32_t color2);
-    }
+        static uint32_t lerpRGB(RGB color1, RGB color2, double scale);
+
+        /**
+         * @brief Converts RGB to HSV
+         *
+         * @param rgb The RGB struct to convert from
+         * @return hsv A HSV struct
+         */
+        static HSV RGBtoHSV(RGB rgb);
+
+        /**
+         * @brief Converts HSV to RGB
+         *
+         * @param hsv The HSV struct to convert from
+         * @return A HSV struct
+         */
+        static RGB HSVtoRGB(HSV hsv);
+
+        /**
+         * @brief uint32_t Color to RGB
+         *
+         * @param color the Color to Convert
+         * @return RGB
+         */
+        static RGB UINT32toRGB(uint32_t color);
+
+        /**
+         * @brief uint32_t Color to RGB
+         *
+         * @param color the Color to Convert
+         * @return HSV
+         */
+        static HSV UINT32toHSV(uint32_t color);
+
+        /**
+         * @brief Converts RGB to HSV
+         *
+         * @param rgb The RGB struct to convert from
+         * @return A uint32_t color
+         */
+        static uint32_t RGBtoUINT32(RGB rgb);
+
+        /**
+         * @brief Converts HSV to RGB
+         *
+         * @param hsv The HSV struct to convert from
+         * @return A uint32_t color
+         */
+        static uint32_t HSVtoUINT32(HSV hsv);
+    };
 }
