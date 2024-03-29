c71a1dab97d9bf1c961edf315bc7c60cfd8f9ac7
ABUCKY0 <81783950+ABUCKY0@users.noreply.github.com>
2024-03-28 22:40:14 -0500

    Setup

---
+++ src/LedLib/effects/TurnSignal.cpp
@@ -0,0 +1,13 @@
+// Include Effect
+#include "LedLib/effects/TurnSignal.hpp"
+#include "LedLib/LedLib.hpp"
+#include "main.h"
+
+/// @see https://github.com/Robiv8/TurnSignal-WS2812B/blob/master/BlinkerTestButtonFinal.ino#L66
+
+using namespace std;
+namespace LedLib
+{
+    void TurnSignal::setup(LedLib &ledLib) {return; /* No setup required*/};
+    void TurnSignal::update(LedLib &ledLib) {}
+};
